4b73bf4f0dc03a1050af08f8884b90d7e91fe26a
Change Me <changeme@bnl.gov>
2023-08-03 19:45:33 -0400

    nothing

--- tpot_felix_dma_buffer_reset.py
+++ tpot_felix_dma_buffer_reset.py
@@ -2,7 +2,7 @@
 
 from sphenix_pytpc import fee
 import sphenix_pytpc.damserv_grpc_client as client
-d = client.Dam(0, 'localhost:50051')
 
 # FIFO reset
+d = client.Dam(0, 'localhost:50051')
 d.reset()
