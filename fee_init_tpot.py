db34aae5597f4e46f7b61d0f32eddcacb8595cf7
mchiu-bnl <chiu@bnl.gov>
2024-04-30 11:23:16 -0400

    - update to new, temporary FEE config script from John K. - since the script uses ssh to ebdc39, it does not need dedicated python. Use #!/usr/bin/env python3 instead.

--- fee_init_tpot.py
+++ fee_init_tpot.py
@@ -1,19 +1,27 @@
-#!/opt/venvs/sphenix-pytpc/bin/python
+#!/usr/bin/env python3
 
 import subprocess
 import re
 import argparse
 
 def main():
-    fee_init_base_command = '/opt/venvs/sphenix-pytpc/bin/fee_init sampa --pre-samples 103 --samples 50 --shape-gain 6'
+    # fee_init_base_command = '/opt/venvs/sphenix-pytpc/bin/fee_init sampa --pre-samples 103 --samples 50 --shape-gain 6'
 
     # first run full command on all TPOT FEEs
-    fee_init_command = fee_init_base_command + ' --region TPOT'
+    # fee_init_command = fee_init_base_command + ' --region TPOT'
+
+
+    # new temporary initialization command from John K. (April 29, 2024)
+    fee_init_command = '/home/kuzew/fee_config.py triggered  --pre-samples 103 --samples 50 --shape-gain 6'
+
     print( 'fee_init_command: ', fee_init_command )
     
     result = subprocess.run( ['ssh', 'ebdc39', '-x', fee_init_command], stdout=subprocess.PIPE)
     output = result.stdout.decode('utf8');
-    # print( output )
+    print( output )
+
+    # for now (April 29, 2024), per FEE initialization does not work.
+    return
 
     # now run over all FEEs independantly, up to five times or untill there is no error message
     # these are TPOT links
