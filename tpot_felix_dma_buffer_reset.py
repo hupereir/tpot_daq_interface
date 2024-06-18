0f8b633c87f23769bca1877e581e6335e6e867ed
mchiu-bnl <chiu@bnl.gov>
2024-06-17 22:24:16 -0400

    removed obsolete script

--- tpot_felix_dma_buffer_reset.py
+++ tpot_felix_dma_buffer_reset.py
@@ -3,6 +3,8 @@
 from sphenix_pytpc import fee
 import sphenix_pytpc.damserv_grpc_client as client
 
+print( "running tpot_felix_dma_buffer_reset.py" )
+
 # FIFO reset
 d = client.Dam(0, 'localhost:50051')
 d.reset()
