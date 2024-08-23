d32674c261cea88330d51fd5b322f111c436892c
mchiu-bnl <chiu@bnl.gov>
2024-08-23 12:12:22 -0400

    cleanup

--- tpot_fee_init.py
+++ tpot_fee_init.py
@@ -5,7 +5,6 @@ import re
 import argparse
 
 def main():
-    # first run full command on all TPOT FEEs
     # fee_init_command = '/home/phnxrc/operations/TPOT/tpot_daq_interface/fee_init_local triggered --connect-tpot --pre-samples 86 --samples 25 --shape-gain 6 --no-stream-enable'
 
     threshold_file = '/home/phnxrc/operations/TPOT/tpot_daq_interface/TPOT_thresholds.json'
@@ -19,14 +18,5 @@ def main():
     output = result.stdout.decode('utf8');
     print( output )
 
-    # enable FEE 9 and 23
-    # fee_init_command = '/home/phnxrc/operations/TPOT/tpot_daq_interface/fee_init_local  stream_enable --fee 9 23'
-
-    # print( 'fee_init_command: ', fee_init_command )
-    # result = subprocess.run( ['ssh', 'ebdc39', '-x', fee_init_command], stdout=subprocess.PIPE)
-    # output = result.stdout.decode('utf8');
-    # print( output )
-  
-    
 if __name__ == '__main__':
   main()
